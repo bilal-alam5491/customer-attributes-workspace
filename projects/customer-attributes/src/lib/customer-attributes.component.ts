e06503cc6241bc31722bde9c4f7d57391c1a1506
Bilal Alam <bilal.alam5491@gmail.com>
2025-08-26 15:01:26 +0500

    api call test

--- projects/customer-attributes/src/lib/customer-attributes.component.ts
+++ projects/customer-attributes/src/lib/customer-attributes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input, Output, EventEmitter } from '@angular/core';
+import { CustomerAttributesService } from './customer-attributes.service';
 
 interface Customer {
   id: number;
@@ -15,6 +16,12 @@ interface Customer {
   styleUrls: ['./customer-attributes.component.scss'],
 })
 export class CustomerAttributesComponent implements OnInit {
+  // New input/output properties for custom data communication
+  @Input() customData: any;
+  @Output() dataToHost = new EventEmitter<any>();
+
+  ccmChannels: any[] = [];
+
   @Input() customers: Customer[] = [
     {
       id: 1,
@@ -42,9 +49,65 @@ export class CustomerAttributesComponent implements OnInit {
     }
   ];
 
-  constructor() { }
+  @Output() customerSelected = new EventEmitter<Customer>();
+  @Output() customersChanged = new EventEmitter<Customer[]>();
+
+  constructor(private customerAttributesService: CustomerAttributesService) { }
 
   ngOnInit(): void {
+    this.updateFQDNFromCustomData();
+    this.getCCMChannels()
+  }
+
+  getCCMChannels() {
+    this.customerAttributesService.getCcmChannels().subscribe({
+      next: (data) => {
+        console.log('Fetched CCM Channels:', data);
+        this.ccmChannels = data;
+      },
+      error: (error) => {
+        console.error('Error fetching CCM Channels:', error);
+      }
+    });
+  }
+
+  private updateFQDNFromCustomData(): void {
+    if (this.customData) {
+      console.log('Received data from host:', this.customData);
+
+      if (this.customData?.FQDN) {
+        this.customerAttributesService.setFQDN(this.customData.FQDN);
+      } else {
+        console.warn('No FQDN found in customData');
+      }
+
+      this.sendDataToHost({
+        message: 'Data received successfully',
+        timestamp: new Date(),
+      });
+    }
+  }
+
+  // Method to send data to host
+  sendDataToHost(data: any): void {
+    console.log('Sending data to host:', data);
+    this.dataToHost.emit(data);
+  }
+
+  onCustomerSelect(customer: Customer): void {
+    this.customerSelected.emit(customer);
+    // Also send additional data about the selection
+    this.sendDataToHost({
+      event: 'customerSelected',
+      timestamp: new Date(),
+      selectedCustomer: customer,
+      totalCustomers: this.customers.length
+    });
+  }
+
+  updateCustomers(newCustomers: Customer[]): void {
+    this.customers = [...newCustomers];
+    this.customersChanged.emit(this.customers);
   }
 
   getStatusClass(status: string): string {
--- projects/customer-attributes/src/lib/customer-attributes.service.ts
+++ projects/customer-attributes/src/lib/customer-attributes.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
-
+import { catchError, Observable, throwError } from 'rxjs';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+  HttpParams,
+} from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerAttributesService {
+  private FQDN: string = '';
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
+
+  setFQDN(fqdn: string) {
+    this.FQDN = fqdn;
+    console.log('FQDN set in service:', this.FQDN);
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    return throwError(errorResponse);
+  }
+
+  getCcmChannels(): Observable<any> {
+    return this.httpClient
+      .get<any>(`${this.FQDN}/ccm/channel-types`, {
+        headers: new HttpHeaders({
+          "Content-Type": "application/json",
+        }),
+      })
+      .pipe(catchError(this.handleError));
+  }
 }
